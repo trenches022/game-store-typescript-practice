03d0d080147c67c433fe26323a3457ae207524cf
./trenches <romanvagner@mail.com>
2025-02-10 15:08:44 +0100

    Added new features, changed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import "./App.css";
 import { GamesFromServer } from "./data";
 import Game from "./Game";
 import AdultOrNot from "./AdultQuestion";
+import Header from "./Header/Header";
+import Cart from "./Cart/Cart";
 import { getGamesFromServer } from "./utils";
+
+function MainContent({ games, isAdult }: { games: GamesFromServer; isAdult: boolean }) {
+  const location = useLocation();
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+
+      {location.pathname !== "/cart" && (
+        <div className="store-container">
+          <h1 className="store-title">Game Store</h1>
+          <div className="game-list">
+            {games.map((game) => (
+              <div key={game.id} className="game-wrapper">
+                {game.forKids && !isAdult ? (
+                  <div className="restricted">This game is not available to you</div>
+                ) : (
+                  <Game game={game} />
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
 
 function App() {
   const [isAdult, setIsAdult] = useState<boolean | null>(null);
@@ -26,20 +59,9 @@ function App() {
   }
 
   return (
-    <div className="store-container">
-      <h1 className="store-title">Game Store</h1>
-      <div className="game-list">
-        {games.map((game) => (
-          <div key={game.id} className="game-wrapper">
-            {game.forKids && !isAdult ? (
-              <div className="restricted">This game is not available to you</div>
-            ) : (
-              <Game game={game} />
-            )}
-          </div>
-        ))}
-      </div>
-    </div>
+    <Router>
+      <MainContent games={games} isAdult={isAdult} />
+    </Router>
   );
 }
 
